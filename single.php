9fba737b51c116ebfc15c4acbf8484cedef8e186
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-03-15 19:22:13 +0100

    Add disqus instead of WP comments template

--- single.php
+++ single.php
@@ -10,6 +10,16 @@
   <div class="single-post-content-wrapper">
     <article class="single-post-content">
       <?php the_content(); ?>
+      <div id="disqus_thread"></div>
+    <script>
+    (function() {
+    var d = document, s = d.createElement('script');
+    s.src = 'https://dszczepaniak.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
     </article>
     <?php get_sidebar(); ?>
   </div>
@@ -20,10 +30,6 @@
       <a class="err-page-link" href="<?php echo home_url(); ?>">strona główna</a>
     </div>
   <?php endif; ?>
-  <div class="">
-    <h3>Komentarze:</h3>
-    <?php comments_template(); ?>
-  </div>
 </section>
 </div>
 <?php get_footer(); ?>
